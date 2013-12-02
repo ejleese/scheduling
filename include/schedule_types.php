7578245fde3464f3f32424f3f80cd22df96f7afa
root <root@bmc0904.borisch.local>
2013-12-02 10:42:03 -0500

    initial commit

---
+++ include/schedule_types.php
@@ -0,0 +1,46 @@
+<!-- schedule_types.php -->
+<!-- purpose: translate 3 letter schedule type codes into long desc -->
+<!-- usage: included at the top of prod_login.php to deal with passed param -->
+<!-- Note: DO NOT DUPLICATE ANY TYPE CODES IN USE IN NOGALES! (to make easier to merge later if needed) -->
+
+<?php
+
+# key-value pair format: "XXX"=>"Description_Text"
+# where "XXX" is a unique, 3-character department abbreviation, and
+# where "Description_Text" is the description of that department.
+# Note: Use underscores rather than spaces (passed as parameter)
+# The order of pairs determines order they show up on scheduling.php list
+
+$schedules= array("SRC"=>"Source",
+									"TSD"=>"Test_Defense",
+									"TSA"=>"Test_Aerospace",
+									"SOL"=>"Select_Solder",
+									"WSO"=>"Wave_Solder",
+									"COT"=>"Coating");
+
+function getDesc($type)
+{
+ 	global $schedules; 
+  $return_val=$schedules[$type];
+	if ($return_val == null) return "UNKNOWN";
+	else return $return_val;	
+}
+
+function genList()
+{
+	global $schedules;
+
+	
+	echo "<table class='headertable'>";
+
+	foreach ($schedules as $type=>$type_long)
+	{
+		echo "<tr><td><a href='/scheduling/prod_login.php?sched=",$type,"'>",$type_long,"</a></td></tr>";
+	}
+
+	echo "</table>";	
+
+	return true;
+}
+?>
+
---
+++ scheduling.php
@@ -0,0 +1,21 @@
+<!-- scheduling.php  -  main scheduling page -->
+<!-- purpose: display menu for navigation -->
+
+<html>
+<?php include 'include/schedule_types.php'; ?>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="/scheduling/include/generalstyle.html">
+		<link rel="stylesheet" type="text/css" href="/scheduling/include/headermenustyle.html">
+	</head>
+	<title>Scheduling</title>
+	<body>
+
+		<div align="center">
+			<h1>Select a Schedule below to view / edit</h1>
+
+			<?php $genTable = genList(); ?>
+
+		</div>
+	</body>
+</html>
