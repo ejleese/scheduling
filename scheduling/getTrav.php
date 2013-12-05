c1751d5da50e03c901150734ff302c3d64836169
root <root@filepro.nogales.borisch.local>
2013-12-05 05:07:07 -0700

    removed duplicate dir

--- scheduling/getTrav.php
+++
@@ -1,9 +0,0 @@
-<?php
-// getTrav.php
-// usage: called by sched.js
-// purpose: calls script that calls filepro report to fetch traveler data
-// author: Eric Leese 9/11/13
-
-$trav = $_GET['trav'];
-passthru("/appl/www/cgi-bin/sched_getTrav.sh '$trav'");
-?>
